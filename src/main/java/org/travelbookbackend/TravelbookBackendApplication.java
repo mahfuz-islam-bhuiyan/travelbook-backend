48d152dd205f8c6ab9db8720216451e36e0456c5
mahfuz-islam-bhuiyan <mahfuz100191@gmail.com>
2020-09-26 16:13:23 +0600

    Initial commit

---
+++ src/main/java/org/travelbookbackend/TravelbookBackendApplication.java
@@ -0,0 +1,13 @@
+package org.travelbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbookBackendApplication.class, args);
+	}
+
+}
