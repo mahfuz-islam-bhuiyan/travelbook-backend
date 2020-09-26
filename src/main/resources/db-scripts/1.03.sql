b147aea0e64dd073ed58e582b54ffb8fc3e8b1da
mahfuz-islam-bhuiyan <mahfuz100191@gmail.com>
2020-09-27 03:05:31 +0600

    updated 1.03.sql with location insert queries

--- src/main/resources/db-scripts/1.03.sql
+++ src/main/resources/db-scripts/1.03.sql
@@ -2,6 +2,13 @@ alter table user modify email varchar(70) null;
 
 alter table user modify password varchar(100) null;
 
+INSERT INTO location(location) VALUES ('Dhaka');
+INSERT INTO location(location) VALUES ('CTG');
+INSERT INTO location(location) VALUES ('Sylhet');
+INSERT INTO location(location) VALUES ('Rajshahi');
+INSERT INTO location(location) VALUES ('Coxs Bazar');
+INSERT INTO location(location) VALUES ('Bandarban');
+
 -- ----------------------
 -- Version Stamp
 -- ----------------------
